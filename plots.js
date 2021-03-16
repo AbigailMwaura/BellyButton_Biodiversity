875194ba95140606ec48b7f0b47077c236932ba7
Abigail Mwaura <amwaura60@gmail.com>
2021-03-16 11:58:30 -0400

    Challenge

--- plots.js
+++ plots.js
@@ -41,7 +41,7 @@ function buildMetadata(sample) {
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
-    // Use `.html("") to clear any existing metadata
+    // use `.html("") to clear any existing metadata
     PANEL.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
